9b17845af83f4fdfdb4e0c964a7d4221279d747a
omf2333 <1923208364@qq.com>
2020-06-19 01:01:45 +0800

    add enemy ScriptableObject to store enemy attributes

---
+++ Assets/Scripts/Enemy/EnemyData.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "BaseEnemyAttributes.asset", menuName = "Enemies/BaseEnemyAttributes")]
+public class EnemyData : ScriptableObject
+{
+    public EnemyType enemyType;
+
+    public float health;
+
+    public float speed;
+
+    public float attackRange;
+
+    public GameObject deathEffect;
+
+    public GameObject damageTextPf;
+}
