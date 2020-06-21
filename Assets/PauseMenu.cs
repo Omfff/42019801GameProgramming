63b7c41f6eee15e565e6c6287d53dc8ed4a4f2ee
Luoyu <1197182168@qq.com>
2020-06-22 01:12:40 +0800

    Add death menu

--- Assets/PauseMenu.cs
+++ Assets/PauseMenu.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class PausuMenu : MonoBehaviour
+public class PauseMenu : MonoBehaviour
 {
     public static bool GameIsPaused = false;
 
