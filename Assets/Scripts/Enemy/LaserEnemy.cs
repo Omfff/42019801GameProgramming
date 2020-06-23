a4d655e47577b15a3cc3296b79f8189c0071c11f
omf2333 <1923208364@qq.com>
2020-06-24 00:17:09 +0800

    add Gear tag and laser avoid hitting gear

--- Assets/Scripts/Enemy/LaserEnemy.cs
+++ Assets/Scripts/Enemy/LaserEnemy.cs
@@ -97,7 +97,8 @@ public class LaserEnemy:FollowingEnemyAi
 
                     hitInfo = hitInfoList[i];
                     if (hitInfo.transform.tag == "Enemy" || hitInfo.transform.tag == "Bullet" || hitInfo.transform.tag == "Shield"
-                        || hitInfo.transform.tag == "Swamp" || hitInfo.transform.tag == "Item" || hitInfo.transform.tag == "Spike")
+                        || hitInfo.transform.tag == "Swamp" || hitInfo.transform.tag == "Item" || hitInfo.transform.tag == "Spike"
+                        || hitInfo.transform.tag == "Gear")
                     {
                         continue;
                     }
